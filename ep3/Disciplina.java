6035f29ce0e81ef7ce3ac79acd0a0ad28b73c610
RodrigoF10 <rodrigofaria50@hotmail.com>
2017-03-13 12:25:23 -0300

    Pasta nota e disciplina adicionadas

---
+++ ep3/Disciplina.java
@@ -0,0 +1,21 @@
+public class Disciplina{
+	private String nome;
+	private String professor;
+
+
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+
+	public void setProfessor(String professor){
+		this.professor = professor;
+	}
+
+	public String getNome(String nome){		
+		return nome;
+	}
+
+	public String getProfessor(String nome){
+		return professor;
+	}
+
---
+++ ep3/Nota.java
@@ -0,0 +1,15 @@
+public class{
+	private double valor;
+	private Alunos aluno;
+	private Disciplina disciplina;
+	
+	public void setValor(double valor){
+		this.valor = valor;
+	} 
+	public Alunos setAluno(Alunos aluno){
+		this.aluno = aluno;	
+	}
+	public Discilina setDisciplina(Disciplina disciplina){
+		this.disciplina = disciplina;
+	}
+}
